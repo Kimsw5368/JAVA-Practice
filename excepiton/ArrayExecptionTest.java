699af68d562778ee01633ffb81557d091899e17c
김성욱 <70447614+Kimsw5368@users.noreply.github.com>
2021-01-28 21:12:52 +0900

    예외처리

---
+++ excepiton/ArrayExecptionTest.java
@@ -0,0 +1,18 @@
+package excepiton;
+
+public class ArrayExecptionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[5];
+		try {
+			for(int i=0; i<=5; i++) {
+				System.out.println(arr[i]);
+			}
+		}
+			catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println(e);
+		}
+	}
+
+}
---
+++ excepiton/ThrowsException.java
@@ -0,0 +1,25 @@
+package excepiton;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+	public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException {
+		FileInputStream fis = new FileInputStream(fileName);
+		Class c = Class.forName(className);
+		return c;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThrowsException test = new ThrowsException();
+		
+		try {
+			test.loadClass("b.txt","java.lang.string");
+		}catch (FileNotFoundException e) {
+			System.out.println(e);
+		}catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}
+	}
+
+}
